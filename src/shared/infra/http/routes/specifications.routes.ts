0ed55aad6a58d8115de16fa1bbb32e6194a884d4
Caio Pratali <caio@pratali.com.br>
2022-07-19 21:21:30 -0300

    ref imports

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
-import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
