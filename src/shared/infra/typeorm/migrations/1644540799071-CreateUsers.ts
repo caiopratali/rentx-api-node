9a92cb55edbbf456e5eb24b9ed2e1ce68e99f717
Caio Pratali <caio@pratali.com.br>
2022-07-23 12:49:23 -0300

    create integration tests category controller

--- src/shared/infra/typeorm/migrations/1644540799071-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1644540799071-CreateUsers.ts
@@ -9,11 +9,11 @@ export class CreateUsers1644540799071 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
             type: "varchar",
-            isPrimary: true,
           },
           {
             name: "username",
